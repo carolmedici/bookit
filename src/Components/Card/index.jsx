2c42286f3caf079a0aa86597e1876340a6745c78
carolmedici <carolina.obstetriz@gmail.com>
2023-04-29 22:45:38 -0300

    :lipstick: layout card

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -6,6 +6,7 @@ const ProductCard = ({nameType, amount, description, id, className}) =>{
     return(
         <div className={`product-card ${className}`}>
             <Item nameType={nameType} amount={amount} description={description} id={id} />
+           
         </div>
     )
 }
@@ -17,9 +18,11 @@ const Card = () =>{
         <>
        <div className="flex m-5 p-5 lg:text-2xl sm:text2xl justify-center ">
            
-       <ProductCard nameType="Maldives" amount="$250"  className="text-center m-1 p-4 bg-emerald-400 rounded-2xl w-80" id="1" />
-      <ProductCard nameType="Hawaii" amount="$300" className="text-center p-4 m-1  bg-teal-400 rounded-2xl w-80" id="2" />
-      <ProductCard nameType="Acropolis" amount="$350" className="text-center p-4 m-1  bg-cyan-400 rounded-2xl w-80" id="3" />
+       <ProductCard nameType="Maldives" description="Discover the exotic beauty of the Maldives, a paradise made up of over 1,000 islands with white sandy beaches and crystal clear waters that invite relaxation. With activities such as snorkeling, diving, and boat tours, this destination offers an unforgettable experience. Perfect for a romantic getaway, the Maldives boast luxurious resorts, sophisticated restaurants, and comfortable accommodations. Book now for a unique and memorable experience!" amount="$250"  className="text-center m-1 p-4 bg-emerald-400 rounded-2xl w-80" id="1" />
+
+      <ProductCard nameType="Hawaii" description="Discover the Pacific paradise with our travel packages to Hawaii! Comprised of eight tropical islands, Hawaii is the perfect vacation destination for those seeking sun, sand, surf, and more. Relax on white sand beaches, savor delicious local cuisine, explore active volcanoes, and marvel at stunning panoramic views. With surfing lessons, scenic hikes, and snorkeling adventures, Hawaii is a paradise for nature and outdoor sports enthusiasts. Book now and discover why Hawaii is one of the most popular tourist destinations in the world!" amount="$300" className="text-center p-4 m-1  bg-teal-400 rounded-2xl w-80" id="2" />
+
+      <ProductCard nameType="Acropolis" description="Experience the Acropolis with our Athens travel packages! This ancient citadel overlooks Athens and is home to the impressive ruins of the Parthenon, Erechtheion, and Temple of Athena Nike. Explore the fascinating history of ancient Greece and admire the stunning architecture and panoramic views of the city. Our exclusive packages offer an authentic Athens experience, including local cuisine and top-rated hotels. Book now and plan your unforgettable trip to the Acropolis!" amount="$350" className="text-center p-4 m-1  bg-cyan-400 rounded-2xl w-80" id="3" />
    
            
        </div>
--- src/Components/Item/index.jsx
+++ src/Components/Item/index.jsx
@@ -1,15 +1,15 @@
-
+import "./Item.css"
 
 const Item = ({ nameType, amount, description, backColor, id }) => {
     return (
       <div className='item'>
         <div className='header' style={{ backgroundColor: backColor }}>
-          <h4>{nameType}</h4>
+          <h4 class="font-bold text-white text-center text-4xl p-2">{nameType}</h4>
         </div>
         <div className='footer'>
-            <p>{description}</p>
-          <h5>{amount}</h5>
-          <button onClick={() => BuyIt(id)}> Buy it
+            <p class="bg-slate-100 rounded-2xl text-justify py-4 px-3 my-2 text-lg">{description}</p>
+          <h5 class="p-4 font-bold">Only: {amount}!</h5>
+          <button class=" font-medium bg-slate-300 px-4 border-4 border-black border-double hover:bg-blue-400" onClick={() => BuyIt(id)}> Buy it
       </button>
         </div>
       </div>
